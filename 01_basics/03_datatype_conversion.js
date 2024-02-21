397e62b61277f57ef7e473c87fffbebbb3a7eeb6
learner-nayan <160713239+learner-nayan@users.noreply.github.com>
2024-02-21 08:06:16 +0000

    conversion of datatypes

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,24 @@
+let num = 10;
+
+console.log(num);
+console.log(typeof num);
+
+let str_num = String(num)
+console.log(str_num);
+console.log(typeof str_num);
+
+let age = "15"
+console.log(age);
+console.log(typeof age);
+
+let num_age = Number(age)
+console.log(num_age);
+console.log(typeof num_age);
+
+
+
+// Number(), String(), Boolean(), Float32Array(), Float64Array()
+
+// While converting (during conversion):- 
+// Boolean(1) = true, Boolean(0) = false 
+// "" = false, "nayan" = true
